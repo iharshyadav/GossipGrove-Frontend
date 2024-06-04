0b1eed71d707a1950d88ce6e14431f05ce894ff3
Harsh Yadav <hy945196@gmail.com>
2024-06-04 06:18:55 +0530

    js

--- backend/server.js
+++ backend/server.js
@@ -14,17 +14,17 @@ app.use(cors({
 
 app.use((req, res, next) => {
   res.header(
-  Access-Control-Allow-Origin,
-  clientURL
+    "Access-Control-Allow-Origin",
+    "https://realtime-webapp.vercel.app"
   );
-  res.header(Access-Control-Allow-Methods, GET, POST, PUT, DELETE, OPTIONS);
-  res.header(Access-Control-Allow-Headers, Content-Type, Authorization);
-  res.header(Access-Control-Allow-Credentials, true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
   
   console.log("Request received:", req.method, req.url);
   
   next();
-  });
+});
 
 const redis = new Redis(process.env.REDIS_CONNECTION_STRING)
 const subRedis = new Redis(process.env.REDIS_CONNECTION_STRING)
@@ -32,7 +32,7 @@ const subRedis = new Redis(process.env.REDIS_CONNECTION_STRING)
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://realtime-webapp.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
