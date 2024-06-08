01d3c08ed3b83488d287c361fff88e904f57c1b1
Harsh Yadav <hy945196@gmail.com>
2024-06-08 20:08:17 +0530

    cors

--- server/routes/otp.route.ts
+++ server/routes/otp.route.ts
@@ -6,9 +6,16 @@ const route = Router()
 
 route.post("/otpVerify",otpSend)
 // route.post("/roomSave",postPrivateRoom)
+const allowedOrigins = ['http://localhost:3000', 'https://realtime-webapp.vercel.app'];
 route.use(cors({
-    origin: ['http://localhost:3000', 'https://realtime-webapp.vercel.app'], // Allow only this origin to access
-    credentials: true, 
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }))
 route.post("/getRoom",getPrivateRoom)
 
