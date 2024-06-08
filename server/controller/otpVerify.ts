f18f302b6ef0b84d56602c5a6736ab7f2cbd34bc
Harsh Yadav <hy945196@gmail.com>
2024-06-08 23:24:16 +0530

    cors3

--- server/controller/otpVerify.ts
+++ server/controller/otpVerify.ts
@@ -100,11 +100,14 @@ export const getPrivateRoom = async (req:Request,res:Response) => {
       throw new Error ("Invalid Otp!!! Please try again!!!!")
       }
 
-    const allowedOrigins = ['https://realtime-webapp.vercel.app', 'http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    // const allowedOrigins = ['https://realtime-webapp.vercel.app', 'http://localhost:3000'];
+    // const origin = req.headers.origin;
+    // if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', 'https://realtime-webapp.vercel.app');
+    // }
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
       
     sendToken(res,findByEmail,200,"user entered successfully");
