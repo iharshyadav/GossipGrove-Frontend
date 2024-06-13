bcc3860020d0af4bdfb4058d0fea98dae0e119ed
Harsh Yadav <hy945196@gmail.com>
2024-06-13 20:14:33 +0530

    build

--- client/src/components/otp.tsx
+++ client/src/components/otp.tsx
@@ -16,8 +16,6 @@ import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { useGlobalContext } from '@/app/Context/store'
 import axios from 'axios'
-import StoreRoom from '@/app/models/rooms.schema'
-import { saveOtpStoreRoom } from '@/app/action'
   
 
 interface otpProps {
